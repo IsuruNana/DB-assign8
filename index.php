18441e58f80835cf3ea715aae958955f1ea4bab5
Isuru Nanayakkara <ipnanayakkara@crimson.ua.edu>
2018-04-19 12:40:42 -0500

    Switched to radio buttons in index.html

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
-    
-
-
+    error_reporting(E_ALL); 
+    ini_set("display_errors", "1");
+    $link=mysqli_connect("cs-sql2014.ua-net.ua.edu", "mybamaID", "CWID");
+    if (!$link) {die("Not connected: ". mysqli_error()); }  // see if connected
+    mysqli_select_db($link, 'login');
+//$query = 'CREATE TABLE testit( '.'id INT NOT NULL, '.'age int)';
+// $result = mysqli_query($link, $query);
+// if(!$result) {die( 'Error in SQL: ' . mysqli_error($link));}
+// echo "table created";
+// mysqli_close($link);
 ?>
+
 
