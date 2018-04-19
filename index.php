87df281c4ca134870b78fecfd0577f4edae70eeb
Isuru Nanayakkara <ipnanayakkara@crimson.ua.edu>
2018-04-19 15:01:35 -0500

    Added radio val

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 </head>
 <body>
     <?php
+        //---------------Setup----------------//
         error_reporting(E_ALL); ini_set("display_errors", "1");
         //  This is example3.php used in previous .htm code
         $link = mysqli_connect("cs-sql2014.ua-net.ua.edu", "smmitchell2", "11192284");
@@ -13,37 +14,36 @@
             die('Not connected: '. mysqli_error()); 
         }
         mysqli_select_db($link, 'smmitchell2') or die ('Could not select database');
+        //---------------End Setup----------------//
 
-        $query = "Select * from customer";
+        $radioVal = $_POST["selectTable"];
+
+        // if($radioVal == "customer") {
+        //     echo("You chose the first button. Good choice. :D");
+        // }
+        // else if ($radioVal == "items") {
+        //     echo("Second, eh?");
+        // }
+
+        $query = "Select * from " . $radioval;
         $result = mysqli_query($link, $query);
 
         //Comment
         if (!$result) {
             die( 'Error in SQL: ' . mysqli_error($link));
         }
-        // process results using cursor
-        while ($row = mysqli_fetch_array($result)) {
+
+        else {
+            // process results using cursor
+            while ($row = mysqli_fetch_array($result)) {
                 echo "<hr>";  //horizontal line
                 echo "id: ". $row["customerID"] . "<br />";
                 echo "name: " . $row["Name"] .  "<br />";
+            }
         }
+        
         mysqli_free_result ($result);
         mysqli_close($link);   // disconnecting from MySQL
-
-        
-        //have 2 host variables
-        // $id= $_POST['id'];
-        // $age = $_POST['age'];
-        // //the query
-        // $query = "insert into  testit values ('$id', '$age')";
-        // $result = mysqli_query($link, $query);
-        // if (!$result) {die('SQL error: ' . mysqli_error($link));}
-        // mysqli_close($link);
-        // print "<html><body><center>";
-        // print "<p>You have just entered this record<p>";
-        // print "ID:  $id<br>";
-        // print "Age: $age";
-        // print "</body></html>";
         
     ?>
 </body>
